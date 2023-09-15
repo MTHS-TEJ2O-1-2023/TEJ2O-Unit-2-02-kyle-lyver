7c47c548282398fc8edf2cccc3c9f3d085cda6b9
kyle-lyver <144264052+kyle-lyver@users.noreply.github.com>
2023-09-15 12:42:44 -0400

    Typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Lyver
+ * Created on: Sep 2023
+ * This program displays "Hello, MTHS!" on a Micro:bit
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
